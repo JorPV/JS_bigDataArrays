74565e961ebf01c0419e7211fede1212bfe9812a
Jordi Prats Verdu <llimonero_4t@hotmail.com>
2022-05-19 14:03:04 +0200

    First commit into draft branch

--- JS/capricorn_women.js
+++ JS/capricorn_women.js
@@ -1,58 +1,90 @@
 // SUB-EXERCISE Capricorn women
 const btnCapricornWomen = document.querySelector(".btn-capricorn_women");
 const filteredPeople = [];
-const birthdayArray = [];
-// filter Capricorn
+const birthdaysArray = [];
+
+// 1. Get the birthdays
 for (let {birthday: {dmy: d}} of randomPersonData) {
-  capricornArray.push(d);
-};
-console.log(birthdayArray);
+  if (d)
+  birthdaysArray.push(d);
+  };
+console.log(birthdaysArray); 
+
+
+// 2.filter Capricorn
+const startDate = new Date ('December 22');
+console.log(startDate)
+const endDate = new Date ("January 19");
+console.log(endDate)
+
+
+const getCapricorn = birthdaysArray.forEach(date => Date.parse(date));
+console.log (getCapricorn);
+
+
+// const filterCapricorn = (person) => {
+//   const month = parseInt(person.birthday.dmy.split("/")[1]);
+//   console.log(month); // 1..12
+//   const day = parseInt(person.birthday.dmy.split("/")[0]); // 1..31
+//   let result = birthdaysArray.filter(e => new Date (e) >= sDate && new Date (e) <= eDate);
+//   return result;
+// };
+
+// console.log(filterCapricorn())
+
+const isCapricorn = (person) => {
+  const dmy = person.birthday.dmy;
+  return dmy;
+}
+console.log(isCapricorn())
 
 
 // filter women, aged 30 or older and Capricorn
-const womanArray = randomPersonData.filter(function (person) {
-  return person.title == "mrs" && person.age >= 30 && person.birthday;
+const woman30 = randomPersonData.filter(function (person) {
+  // const month = parseInt(person.birthday.dmy.split("/")[1]); // 1..12
+  // console.log(month);
+  // const day = parseInt(person.birthday.dmy.split("/")[0]); // 1..31
+  return person.gender === "female" && person.age >= 30 //&& month >= startDate || person.birthday.dmy <= endDate;
 });
-console.log(womanArray);
+// sort women by name
+const sortWomenByName = () => {
+  return woman30.sort((a,b) => (a.name > b.name ? 1: -1)); 
+};
 
-// Get an name, surname and photo data
-function listOfWoman() {
-  const ulCapriWoman = document.createElement("ul");
-  womanArray.map((woman) => {
-    const liWoman = document.createElement("li")
-    const img = document.createElement("img");
-      img.src = woman.photo;
-      liWoman.appendChild(img);
-      ulCapriWoman.appendChild(liWoman); 
-    if (!filteredPeople.includes(woman)) {
-      filteredPeople.push(woman.name + " " + woman.surname + " " + img.src);
-    }
-    return filteredPeople.sort(); //  Sort people by name
-  });
-  console.log(filteredPeople);
-}
+console.log(sortWomenByName())
 
-listOfWoman();
 
-// Add list of people to DOM
- const ulCapriWomen = document.createElement("ul");
-const addlistToDom = filteredPeople.forEach((person) => {
-  // create html elements
-  const liCapriWomen = document.createElement("li");
-  liCapriWomen.innerHTML = person;
-  ulCapriWomen.appendChild(liCapriWomen);
+// // Get the name, surname and photo data
+// const listOfWomen = function() {
+//   const ulCapriWoman = document.createElement("ul");
+//   womanArray.map((woman) => {
+//     const liWoman = document.createElement("li")
+//     const img = document.createElement("img");
+//       img.src = woman.photo;
+//       liWoman.appendChild(img);
+//       ulCapriWoman.appendChild(liWoman); 
+//     if (!filteredPeople.includes(woman)) {
+//       filteredPeople.push(woman.name + " " + woman.surname + " " + img.src);
+//     }
+//     return filteredPeople.sort(); //  Sort people by name
+//   });
+//   console.log(filteredPeople);
+// }
 
-  // if (buttonStatus === false) {
-  //   ulCountries.style.visibility = "hidden";
+// console.log(listOfWomen());
 
-  //   buttonStatus = true;
 
-  // } else if (buttonStatus == true) {
-  //   ulCountries.style.visibility = "visible";
 
-  //   buttonStatus = false;
-  // }
-});
+
+
+// // Add list of people to DOM
+// const ulCapriWomen = document.createElement("ul");
+// const addlistToDom = filteredPeople.forEach((person) => {
+//   // create html elements
+//   const liCapriWomen = document.createElement("li");
+//   liCapriWomen.innerHTML = person;
+//   ulCapriWomen.appendChild(liCapriWomen);
+// });
 
 // const listOfPhotos = document.querySelector("#container");
 
